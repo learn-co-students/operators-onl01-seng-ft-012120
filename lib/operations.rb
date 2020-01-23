3bfa30f3cc4927def0e57ec1acd5a01bc20eed0f
Khalid T <khalid.tissire@gmail.com>
2020-01-23 08:03:42 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,13 @@
 def unsafe?(speed)
-
+  if speed.to_i.between?(40,60)
+    return false
+  else
+    return true
+  end
 end
 
-
-
 def not_safe?(speed)
-	
+	return speed.to_i.between?(40,60) ? false : true
 end
 	
 
