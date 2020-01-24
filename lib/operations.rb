2774724cac1fff5ad89c55753c5496ac433134f1
Jessie Tivey <jesstivey@gmail.com>
2020-01-24 20:23:03 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,19 @@
 def unsafe?(speed)
-
+  if speed < 40 
+    "safe"
+  elsif speed > 60
+    "safe"
+  else
+    "unsafe"
+  end   
 end
 
 
 
 def not_safe?(speed)
-	
+  safe ? 
 end
 	
+conditional ? action_if_true : action_if_false
 
 
