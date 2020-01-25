ac3ab54e779016a1f5c855b2e72e6094b0f705be
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-25 13:31:47 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,15 @@
 def unsafe?(speed)
-
+  unsafe = "40"
+  puts "{unsafe}" if unsafe < 40
+end
 end
 
 
 
 def not_safe?(speed)
+  not_safe = "60"
+ puts "{not_safe}" > "60" ? : "{safe}"
+ end
 	
 end
 	
